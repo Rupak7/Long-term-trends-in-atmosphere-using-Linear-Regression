d26c631d28bc93c51f72eb5c106324d141ea520c
Rupak.joko7 <rupakkalita3@gmail.com>
2018-07-31 17:34:46 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
 nc=Dataset('/home/rupak7/SABER_Temp_O3_April2002_v2.0.nc','r')
 print nc  #summary of the netCDF file
 
 print(nc.variables.keys())  #get all varibale names
 lat=nc.variables['tplatitude'][:]  #latitude variable
-#print lat
+print lat
 
 orb=nc.variables['orbit'][:]
 print orb
@@ -15,9 +18,19 @@ for d in nc.dimensions.items():  #values of the dimensions
   	print(d)
 
 
-# #every attribute has its own dimension and shape
+#every attribute has its own dimension and shape
 print lat.dimensions
 print lat.shape
 
 # #each dimension typically has a variable associated with it
 
+fig = plt.figure(num=None, figsize=(12, 8) )
+m = Basemap(projection='merc',llcrnrlat=-80,urcrnrlat=80,llcrnrlon=-180,urcrnrlon=180,resolution='c')
+m.drawcoastlines()
+m.fillcontinents(color='tan',lake_color='lightblue')
+# draw parallels and meridians.
+m.drawparallels(np.arange(-90.,91.,30.),labels=[True,True,False,False],dashes=[2,2])
+m.drawmeridians(np.arange(-180.,181.,60.),labels=[False,False,False,True],dashes=[2,2])
+m.drawmapboundary(fill_color='lightblue')
+plt.title("Mercator Projection")
+plt.show()
